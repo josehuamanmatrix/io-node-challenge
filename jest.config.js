b949eb851e6ba4cd94a4bf8cf059092c595a8260
Jose Huaman <jose.huaman.matrix@gmail.com>
2024-07-31 13:08:58 -0500

    feat: Add husky

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: "node",
+  collectCoverage: true,
+  verbose: true,
+  testMatch: ["**/*.(test|steps).+(ts|tsx|js)"],
+  setupFiles: ["dotenv/config"],
+  coveragePathIgnorePatterns: ["/node_modules/", "/test/", "/dist/"],
+  modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: false,
+        testEnvironment: "node",
+        collectCoverage: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "src/(.*)": "<rootDir>/src/$1",
+  },
+};
